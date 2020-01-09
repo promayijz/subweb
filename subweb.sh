085bd4f70670e611c71a340666a39a9a6602baa4
promayi <promayi@aliyun.com>
2020-01-09 15:43:56 +0800

    update by jz - update start process

--- subweb.sh
+++ subweb.sh
@@ -1,35 +1,19 @@
 #! /bin/sh 
 echo "-----------------------------------Start-------------------------------------"
-#echo "-----------------------------------$1,$2------------------------"
-#filename="/root/subweb/api/aff.py"
-#cat>"${filename}"<<EOF
-#aff = '不限制机场，规则生成失败，请检测调用格式。STC测试可用，注册地址：bilibili.stchk.cloud/auth/register?code=gzI5'   
+filename="/root/subweb/api/aff.py"
+cat>"${filename}"<<EOF
 #apiip = '$1'
-#subip = '$2'               
-#EOF
-#cat /root/subweb/api/aff.py
-chmod 777 /root/subweb/config/subconverter
-#echo "-----------------------------------Update file----------------------------------------"
-#echo "-------------------------------------While--------------------------------------------"
-while true
-do
-    proc_name="subconverter"        #进程名
-    proc_num()                      #查询进程数量
-    {
-        num=`ps -ef | grep $proc_name | grep -v grep | wc -l`
-        return $num
-    }
-    proc_num  
-    number=$?                       #获取进程数量
-    if [ $number -eq 0 ]            #如果进程数量为0
-    then                            #重新启动服务器，或者扩展其它内容。
-        cd /root/subweb/config
-        ./subconverter &
-        pkill python3
-        python3 /root/subweb/api.py &   #运行web服务
-#echo "------------------------------------Restart----------------------------------------"
-    fi
-    #sleep 300s
-echo "------------------------------------Sucess----------------------------------------"
-done
-#echo "--------------------------------------WEB-----------------------------------------"
+subip = '$1'               
+EOF
+cat /root/subweb/api/aff.py
+#chmod 777 /root/subweb/config/subconverter
+
+netstat -tupln | egrep '10010|10086'
+
+if [ $? ! -eq 0 ]; then
+    ./stop_subweb.sh
+else
+    cd /root/subweb/config
+    ./subconverter &
+fi
+
